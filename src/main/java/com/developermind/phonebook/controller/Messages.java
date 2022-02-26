6a035d6df49bc5569c620f89072fdaa484bfa26f
ubuntu <ubuntu@ubuntu>
2022-02-26 00:42:49 -0800

    fixed compile error due to java version change

--- src/main/java/com/developermind/phonebook/controller/Messages.java
+++ src/main/java/com/developermind/phonebook/controller/Messages.java
@@ -4,7 +4,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 class Messages {
-	private static final String BUNDLE_NAME = Messages.class.getPackageName() + ".messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = Messages.class.getPackage() + ".messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
